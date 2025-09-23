8fdc26c9be5c6b904470b51f462dee0b4c8026fb
dwendddsss <dewiendng13@gmail.com>
2025-09-23 18:19:29 +0700

    add navbar

---
+++ src/app/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = ({ totalItems }) => {
+  return (
+    <nav className="navbar">
+      <div className="nav-brand">ManagHer Shop</div>
+
+      <ul className="nav-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/products">Shop</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+
+      
+      <div className="nav-cart">
+        Keranjang: <span className="cart-badge">{totalItems}</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
